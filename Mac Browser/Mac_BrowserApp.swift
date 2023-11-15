ef43154fb7c44e8d353038a54703fa8b3d3268a2
Ouze <ossama.benjemaa@gmail.com>
2023-11-15 09:35:38 +0100

    Initial Commit

---
+++ Mac Browser/Mac_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_BrowserApp.swift
+//  Mac Browser
+//
+//  Created by Ossama Ben Jemaa on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Mac_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
